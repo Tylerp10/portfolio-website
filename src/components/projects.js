d58c3c4e6f8cd5d8ab96ce896cf6add0a2dda394
Tylerp10 <tylerpav@gmail.com>
2023-12-14 19:09:00 -0500

    projects update

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Calculator from "../images/calculator.png"
-import RPS from "../images/rps.png"
+import WeatherApp from "../images/weather-app.png"
 
 export default function Projects() {
     return (
@@ -25,19 +25,19 @@ export default function Projects() {
 
                 <div className="project-item">
                     <div className="project-p">
-                    <h4>Rock Paper Scissors</h4>
-                        A simple game of Rock Paper Scissors. The player has the selection to choose one of the three options which is then evaluated against a computers random selection. The winner and updated score is displayed after each round. 
+                    <h4>Weather App</h4>
+                        A weather app built with React that can get the weather from thousands of different cities worldwide. Displays current forecast along with a 5 day forecast that include features such as; humidity, sunrise/sunset, wind speed, pressure, etc. 
                     <div className="project-btns">
                         <button
-                            onClick={()=> window.open("https://soft-dragon-ae7b2e.netlify.app")}
+                            onClick={()=> window.open("https://tyler-weather.netlify.app")}
                             className="demo">Live demo</button>
                         <button 
-                            onClick={()=> window.open("https://github.com/Tylerp10/rock-paper-scissors")}
+                            onClick={()=> window.open("https://github.com/Tylerp10/weather-app")}
                             className="view-code">View code
                         </button>
                     </div>
                     </div>
-                    <img alt="rock paper scissors website" src={RPS}></img>
+                    <img className="weather-app-img" alt="weather app website" src={WeatherApp}></img>
                 </div>
         </div>
     )
