0c8ede24dc8b32eda9e8234ef7ab2e4398969dde
Tylerp10 <tylerpav@gmail.com>
2024-02-09 08:56:51 -0500

    skills update

--- src/components/skills.js
+++ src/components/skills.js
@@ -15,16 +15,56 @@ export default function Skills() {
         <div id="skills" className="skills">
             <h3>Skills</h3>
             <div className="skills-container">
-                <img alt="HTML" src={HTMLimg}></img>
-                <img alt="CSS" src={CSSimg}></img>
-                <img alt="Bootstrap" src={Bootstrap}></img>
-                <img alt="JavaScript" src={JSimg}></img>
-                <img alt="React" src={Reactimg}></img>
-                <img alt="Node.js" src={Nodeimg}></img>
-                <img alt="Mongo" src={Mongo}></img>
-                <img alt="VScode" src={VScodeimg}></img>
-                <img alt="GIT" src={GITimg}></img>
-                <img alt="Figma" src={Figmaimg}></img>
+
+                <figure>
+                    <img alt="HTML" src={HTMLimg}></img>
+                        <h4>HTML</h4>
+                </figure>
+
+                <figure>
+                    <img alt="CSS" src={CSSimg}></img>
+                        <h4>CSS</h4>
+                </figure>
+
+                <figure>
+                    <img alt="Bootstrap" src={Bootstrap}></img>
+                        <h4>Bootstrap</h4>
+                </figure>
+
+                <figure>
+                    <img alt="JavaScript" src={JSimg}></img>
+                        <h4>JavaScript</h4>
+                </figure>
+
+                <figure>
+                    <img alt="React" src={Reactimg}></img>
+                        <h4>React</h4>
+                </figure>
+
+                <figure>
+                    <img alt="Node.js" src={Nodeimg}></img>
+                        <h4>Node</h4>
+                </figure>
+
+                <figure>
+                    <img alt="Mongo" src={Mongo}></img>
+                        <h4>Mongo</h4>
+                </figure>
+
+                <figure>
+                    <img alt="VScode" src={VScodeimg}></img>
+                        <h4>VScode</h4>
+                </figure>
+
+                <figure>
+                    <img alt="GIT" src={GITimg}></img>
+                        <h4>GIT</h4>
+                </figure>
+
+                <figure>
+                    <img alt="Figma" src={Figmaimg}></img>
+                        <h4>Figma</h4>
+                </figure>
             </div>
         </div>
     )
